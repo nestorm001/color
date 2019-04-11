fb09dbcbf65b9d3fbcd35ab42091d3ef9ee423fd
nesto <nestorm1@163.com>
2019-04-11 09:38:09 +0800

    hash a string to color

--- app/src/main/java/com/github/nestorm001/color/MainActivity.kt
+++ app/src/main/java/com/github/nestorm001/color/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.github.nestorm001.color
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        input.addTextChangedListener {
+            it?.run {
+                val string = this.toString()
+                string.printColor()
+                color.setBackgroundColor(string.toColor())
+            }
+        }
+    }
+
+    private fun String.toColor(): Int {
+        return this.hashCode() or 0xFF000000.toInt()
+    }
+
+    private fun String.printColor() {
+        Log.d("wtf", "color of $this is ${Integer.toHexString(toColor())}")
     }
 }
